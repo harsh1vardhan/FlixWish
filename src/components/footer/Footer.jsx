6c14d347060df6f5dfddd8fe850425eca420806e
harsh1vardhan <1harshvardhan9872@gmail.com>
2023-11-01 20:35:18 +0530

    added footer section

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -22,12 +22,7 @@ const Footer = () => {
                     <li className="menuItem">FAQ</li>
                 </ul>
                 <div className="infoText">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                    do eiusmod tempor incididunt ut labore et dolore magna
-                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                    ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                    Duis aute irure dolor in reprehenderit in voluptate velit
-                    esse cillum dolore eu fugiat nulla pariatur.
+                Crafted with unwavering passion and dedication by Harshvardhan, your personal guide to the enchanting world of cinema. FLIXWISH is designed to ignite your love for film and television. Explore, enjoy, and immerse yourself in the magic of the silver screen with us!
                 </div>
                 <div className="socialIcons">
                     <span className="icon">
