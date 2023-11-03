053e6a2f44705c1bb8e947e4bf23330568e0c727
harsh1vardhan <1harshvardhan9872@gmail.com>
2023-11-03 07:37:35 +0530

    added comments

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,10 +1,11 @@
-import axios from "axios";
+import axios from "axios"; // Import the Axios library for making HTTP requests.
 
-const BASE_URL = "https://api.themoviedb.org/3";
-const TMDB_TOKEN = import.meta.env.VITE_APP_TMDB_TOKEN;
+const BASE_URL = "https://api.themoviedb.org/3"; // Define the base URL for the TMDb API.
+const TMDB_TOKEN = import.meta.env.VITE_APP_TMDB_TOKEN;  // Get the API token from environment variables.
+
 
 const headers = {
-    Authorization: "bearer " + TMDB_TOKEN,
+    Authorization: "bearer " + TMDB_TOKEN, // Create an Authorization header with the API token.
 };
 
 export const fetchDataFromApi = async (url, params) => {
@@ -13,9 +14,10 @@ export const fetchDataFromApi = async (url, params) => {
             headers,
             params,
         });
-        return data;
+        return data; // Return the response data if the request is successful.
     } catch (err) {
-        console.log(err);
-        return err;
+        console.log(err);  // Log any errors to the console.
+        return err;  // Return the error object.
+
     }
 };
